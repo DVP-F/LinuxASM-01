ef7712ace1690298c8a6ecde4fd3c764845b86a4
DVP-F <carnx@duck.com>
2025-05-28 00:31:34 +1400

    fixed bubblesortttt

--- sorting/bubblesort.asm
+++ sorting/bubblesort.asm
@@ -6,7 +6,7 @@ section .text
 
 rseed: 
     mov [0x00ff], r10d ; seeding 
-    RET 
+    ret 
 
 rgen: ; xorshift32 implementation (randint gen) 
     mov r10d, [0x00ff] 
@@ -20,90 +20,84 @@ rgen: ; xorshift32 implementation (randint gen)
     shr r11d, 27 
     xor r10d, r11d 
     mov r8d, 2685821657736338717 
-    mul r10d 
+    imul r10d, r8d        ; FIXED: imul instead of mul
     mov [0x00ff], r10d 
-    RET 
+    ret 
 
 _start: 
     call rseed 
-    call rgen 
+
+    ; fill array with 20 random integers
+    mov rcx, 20
+    mov rbx, 0
+.fill_array:
+    call rgen
+    mov [0x0aff + rbx*4], r10d
+    inc rbx
+    loop .fill_array
+
+.sort_outer:
+    mov r12d, 0           ; swap counter
+    mov ecx, 19           ; 20 elements = 19 comparisons
+    xor rbx, rbx
+
+.sort_inner:
+    mov eax, [0x0aff + rbx*4]
+    mov edx, [0x0aff + rbx*4 + 4]
+    cmp eax, edx
+    jbe .no_swap
+
+    ; swap
+    mov [0x0aff + rbx*4], edx
+    mov [0x0aff + rbx*4 + 4], eax
+    inc r12d
+
+.no_swap:
+    inc rbx
+    loop .sort_inner
+
+    cmp r12d, 0
+    jne .sort_outer
 
 post:
     mov rcx, 20              ; 20 numbers to print
     mov rsi, 0x0aff          ; Starting address of numbers
     mov rdi, 1               ; File descriptor (stdout)
 
-L3:
-    mov rax, [rsi]           ; Load the number
+.L3:
+    mov eax, [rsi]           ; Load the number
     call print_number        ; Convert and print it
     add rsi, 4               ; Move to next number (dword)
-    loop L3
+    loop .L3
 
-    mov rax, 1               ; Write syscall (exit) 
-    mov rdi, 0
+    mov rax, 60              ; syscall: exit
+    xor rdi, rdi
     syscall
 
-; Subroutine to print an integer as a string 
 print_number:
-    mov rbx, 10              ; Divisor for decimal
-    mov rdx, 0               ; Clear remainder
-    mov rdi, rsp             ; Use stack for the string buffer
-    add rdi, 40              ; Go to top of stack (safe zone)
-    mov byte [rdi], 0        ; Null-terminate string
+    mov rbx, 10
+    xor rcx, rcx
+    mov rdi, rsp
+    add rdi, 40
+    mov byte [rdi], 0
 
-convert_digit:
-    xor rdx, rdx             ; Clear remainder
-    div rbx                  ; Divide rax by 10
-    add dl, '0'              ; Convert remainder to ASCII
-    dec rdi                  ; Move backwards in buffer
-    mov [rdi], dl            ; Store character
-    test rax, rax            ; If rax is zero, we're done
-    jnz convert_digit
+.convert_digit:
+    xor rdx, rdx
+    div rbx
+    add dl, '0'
+    dec rdi
+    mov [rdi], dl
+    test rax, rax
+    jnz .convert_digit
 
-    mov rax, 1               ; Syscall: write
-    mov rsi, rdi             ; Point to the number string
-    mov rdx, 40              ; Maximum possible digits (safe)
+    mov rax, 1
+    mov rsi, rdi
+    mov rdx, 40
     syscall
 
-    mov rax, 1               ; Print newline
+    mov rax, 1
     mov rsi, newline
     mov rdx, 1
     syscall
 
     ret
-
-    mov CL, 640 ; counter :3 
-L1: 
-    call rgen 
-    mov [0x0aff+CL], [0x00ff] ; set a whole buncha addresses to random ints 
-    call rgen 
-    sub CL, 32 
-    jnz L1 
-
-    mov CL, 20 
-L2: 
-    mov r12d, 0 
-    mov edx, 32 
-    dec CL 
-    mul edx, CL 
-    inc CL 
-
-    mov eax, edx ; prep counters 
-    mov ebx, edx 
-    add ebx, 32 
-
-R1: ; ebx is smaller
-    mov r13d, [0x0aff+ebx] 
-    mov [0x0aff+ebx], [0x0aff+eax] 
-    mov [0x0aff+eax], [0x0aff+ebx] 
-    inc r12d 
-    ret 
-
-    cmp [0x0aff+ebx], [0x0aff+eax] 
-    jb R1 
-
-    dec CL 
-    jnz L2 
-    cmp r12d, 0 
-    je post 
-
